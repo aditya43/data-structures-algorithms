55e849b31a1213d2d387195212e2b1911f7a2d5e
Aditya Hajare <aditya43@gmail.com>
2020-08-12 17:24:27 +0530

    Linked list add()

---
+++ 04-Linked-List/linked-list/LinkedList.js
@@ -0,0 +1,28 @@
+const Node = require('./Node');
+
+class LinkedList {
+    constructor () {
+        this.head = null;
+        this.size = 0;
+    }
+
+    add (value) {
+        const Item = new Node(value);
+
+        if (this.head === null) {
+            this.head = Item;
+        } else {
+            let current = this.head;
+
+            while (current.next) {
+                current = current.next;
+            }
+
+            current.next = Item;
+        }
+
+        this.size++;
+    }
+}
+
+module.exports = new LinkedList();
