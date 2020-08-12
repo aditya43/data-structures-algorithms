9e78cb9a0fd2381ac94ab65e6b9f8ad54b4e5d43
Aditya Hajare <aditya43@gmail.com>
2020-08-12 11:30:26 +0530

    enqueue

--- 03-Priority-Queue/priority-queue/PriorityQueue.js
+++ 03-Priority-Queue/priority-queue/PriorityQueue.js
@@ -6,6 +6,26 @@ class PriorityQueue {
         // An array is used to implement priority
         this.items = [];
     }
+
+    // enqueue function to add element to the queue as per priority.
+    enqueue (item, priority) {
+        const QItem = new QElement(item, priority);
+        let contain = false;
+
+        // Iterating through the entire item array to add element at the correct location of the Queue
+        for (let i = 0; i < this.items.length; i++) {
+            if (this.items[i].priority > QItem.priority) {
+                this.items.splice(i, 0, QItem);
+                contain = true;
+                break;
+            }
+        }
+
+        // if the element have the highest priority it is added at the end of the queue
+        if (!contain) {
+            this.items.push(QItem);
+        }
+    }
 }
 
 module.exports = new PriorityQueue();
