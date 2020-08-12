9c36de5f4441f9a1d8e73f6a1b2d2733c3d4adc0
Aditya Hajare <aditya43@gmail.com>
2020-08-12 11:31:19 +0530

    dequeue method for queue

--- 03-Priority-Queue/priority-queue/PriorityQueue.js
+++ 03-Priority-Queue/priority-queue/PriorityQueue.js
@@ -26,6 +26,16 @@ class PriorityQueue {
             this.items.push(QItem);
         }
     }
+
+    // dequeue method to remove element from the queue
+    dequeue () {
+    // return the dequeued element and remove it. if the queue is empty returns Underflow
+        if (this.isEmpty()) {
+            return 'Underflow';
+        }
+
+        return this.items.shift();
+    }
 }
 
 module.exports = new PriorityQueue();
