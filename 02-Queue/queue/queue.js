094d4ca0fc70c40fbb388aefffeb8b83a3103f05
Aditya Hajare <aditya43@gmail.com>
2020-08-12 07:22:51 +0530

    queue dequeue()

--- 02-Queue/queue/queue.js
+++ 02-Queue/queue/queue.js
@@ -9,6 +9,15 @@ class Queue {
         this.items.push(item);
     }
 
+    // Remove and return element from queue front.
+    dequeue () {
+        if (this.isEmpty()) {
+            return 'Underflow';
+        }
+
+        return this.items.shift();
+    }
+
     // Check if queue is empty.
     isEmpty () {
         return this.items.length === 0;
