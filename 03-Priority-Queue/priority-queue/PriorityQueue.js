8aac3141090d690d62dc3acd2985152cdf09eead
Aditya Hajare <aditya43@gmail.com>
2020-08-12 11:25:11 +0530

    PriorityQueue - Constructor

--- 03-Priority-Queue/priority-queue/PriorityQueue.js
+++ 03-Priority-Queue/priority-queue/PriorityQueue.js
@@ -1,5 +1,11 @@
-class PriorityQueue {
+const QElement = require('./QElement');
 
+// PriorityQueue class
+class PriorityQueue {
+    constructor () {
+        // An array is used to implement priority
+        this.items = [];
+    }
 }
 
 module.exports = new PriorityQueue();
---
+++ 03-Priority-Queue/priority-queue/QElement.js
@@ -0,0 +1,9 @@
+// User defined class to store element and its priority.
+class QElement {
+    constructor (item, priority) {
+        this.item = item;
+        this.priority = priority;
+    }
+}
+
+module.exports = QElement;
