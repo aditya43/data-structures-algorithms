b98d27330180ba895a898a0e75a01b23f7fcb2b6
Aditya Hajare <aditya43@gmail.com>
2020-08-13 21:17:28 +0530

    Keep only unique values in array.

--- 05-Problem-Solving-Patterns/03-Multiple-Pointers-uniqueValues.js
+++ 05-Problem-Solving-Patterns/03-Multiple-Pointers-uniqueValues.js
@@ -1,18 +1,28 @@
 /**
- * Find all unique values in array.
+ * Keep only unique values in array.
  * Write a function called uniqueValues which accepts a sorted array,
- * and count the unique values in the array. There can be negative
+ * and keeps only the unique values in the array. There can be negative
  * values in the array.
  *
  * Time Complexity: O(n)
  * Space Complexity: O(1)
- * Pattern used: Multiple Pointers.
+ * Pattern used: Multiple Pointers (Two pointers at the beginning).
  *
  * @param arr Sorted array of integers
  */
 
 function uniqueValues (arr) {
+    let i = 0;
 
+    for (let j = 1; j < arr.length; j++) {
+        if (arr[i] !== arr[j]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    return arr.slice(0, i + 1);
 }
 
 console.log(uniqueValues([-4, -4, -1, -1, 0, 2, 8]));
+console.log(uniqueValues([]));
