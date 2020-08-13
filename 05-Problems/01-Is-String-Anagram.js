121bc44b11a5038665ab6952b6c6a4ec22648488
Aditya Hajare <aditya43@gmail.com>
2020-08-13 19:27:16 +0530

    Check if string is anagram.

--- 05-Problems/01-Is-String-Anagram.js
+++ 05-Problems/01-Is-String-Anagram.js
@@ -12,23 +12,36 @@
  * @param str2 String 2
  */
 function checkAnagram (str1, str2) {
+    // If strings are not equal, return false.
+    if (str1.length !== str2.length) {
+        return false;
+    }
+
+    // Maintain a frequency lookup object.
     const freq = {};
 
+    // Calculate occurances for each letter in string 1.
     for (const val of str1) {
         freq[val] = (freq[val] || 0) + 1;
     }
 
     for (const val of str2) {
+        // From str2, if the character is not occured once in frequency, return false.
         if (!freq[val] > 0) {
             return false;
         }
 
+        // For the character in str2, decrement occurance frequency by 1.
         if (freq[val] > 0) {
             freq[val] -= 1;
         }
     }
 
+    // String is anagram!
     return true;
 }
 
-console.log(checkAnagram('adia', 'iadx'));
+console.log(checkAnagram('aditya', 'ayt'));
+console.log(checkAnagram('aditya', 'daiyta'));
+console.log(checkAnagram('iceman', 'cinema'));
+console.log(checkAnagram('icemanz', 'cinemay'));
