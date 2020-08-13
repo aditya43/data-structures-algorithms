7a52457525ea4c8f4934ceeecfbe34c462196404
Aditya Hajare <aditya43@gmail.com>
2020-08-13 21:00:44 +0530

    Find all unique values in array.

---
+++ 05-Problem-Solving-Patterns/03-Multiple-Pointers-uniqueValues.js
@@ -0,0 +1,18 @@
+/**
+ * Find all unique values in array.
+ * Write a function called uniqueValues which accepts a sorted array,
+ * and count the unique values in the array. There can be negative
+ * values in the array.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * Pattern used: Multiple Pointers.
+ *
+ * @param arr Sorted array of integers
+ */
+
+function uniqueValues (arr) {
+
+}
+
+console.log(uniqueValues([-4, -4, -1, -1, 0, 2, 8]));
