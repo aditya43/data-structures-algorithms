1846da3b9de14d29114a43807b400ef6dbe8435e
Aditya Hajare <aditya43@gmail.com>
2020-08-13 19:42:59 +0530

    Problem Solving Patterns

---
+++ 05-Problem-Solving-Patterns/02-Multiple-Pointers-sumZero.js
@@ -0,0 +1,12 @@
+/**
+ * Find all pairs in array whose sum is zero.
+ * Write a function called sumZero which accepts a sorted array of integers.
+ * The function should return all pairs where the sum is 0.
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ * Pattern used: Multiple Pointers.
+ *
+ * @param str1 String 1
+ * @param str2 String 2
+ */
