b55d65a76e343c94b67d055f5e7cd117dfa9748f
Aditya Hajare <aditya43@gmail.com>
2020-08-15 10:20:18 +0530

    Optimized bubble sort

--- 06-Searching-Algorithms/04-Bubble-Sort.js
+++ 06-Searching-Algorithms/04-Bubble-Sort.js
@@ -8,13 +8,14 @@
  *
  * @param arr Array of integers
  */
-function bubbleSort (arr) {
+function unOptimizedBubbleSort (arr) {
     if (arr.length < 1) {
         return -1;
     }
 
     for (let i = arr.length; i > 0; i--) {
         for (let j = 0; j < i - 1; j++) {
+            console.log(arr, arr[j], arr[j + 1]);
             if (arr[j] > arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
             }
@@ -24,5 +25,34 @@ function bubbleSort (arr) {
     return arr;
 }
 
-console.log(bubbleSort([4, 1, 5, 2, 9, 3, 4]));
-console.log(bubbleSort([9, 0, 8, -1, -4, 3, 1]));
+/**
+ * Time Complexity After Optimization: O(n)
+ *
+ * @author Aditya Hajare <https://github.com/aditya43>
+ *
+ * @param arr Array of integers
+ */
+function optimizedBubbleSort (arr) {
+    if (arr.length < 1) {
+        return -1;
+    }
+
+    for (let i = arr.length; i > 0; i--) {
+        let noSwaps = true;
+        for (let j = 0; j < i - 1; j++) {
+            console.log(arr, arr[j], arr[j + 1]);
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                noSwaps = false;
+            }
+        }
+        if (noSwaps) {
+            break;
+        }
+    }
+
+    return arr;
+}
+
+console.log(unOptimizedBubbleSort([4, 1, 5, 2, 9, 3, 4]));
+console.log(optimizedBubbleSort([9, 0, 8, -1, -4, 3, 1]));
