d4bab7022f9dbe7b1869779276c60a9737f6c02b
Aditya Hajare <aditya43@gmail.com>
2020-08-16 17:49:09 +0530

    Linked list push() method

--- 07-Singly-Linked-List/linked-list.js
+++ 07-Singly-Linked-List/linked-list.js
@@ -11,6 +11,23 @@ class SinglyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    push (val) {
+        if (val.length < 1) {
+            throw new Error('Empty value passed to push function');
+        }
+
+        const node = new Node(val);
+
+        if (this.head === null && this.tail === null) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+
+        this.length++;
+    }
 }
 
 const list = new SinglyLinkedList();
@@ -19,3 +36,4 @@ list.push('Nishi');
 list.push('John');
 list.push('Jane');
 list.push('Avi');
+console.log(list);
