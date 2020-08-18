35334f97a4c771939a317f4c39dba55c9f087b55
Aditya Hajare <aditya43@gmail.com>
2020-08-18 13:54:33 +0530

    Doubly linked list push() method

--- 08-Doubly-Linked-List/linked-list.js
+++ 08-Doubly-Linked-List/linked-list.js
@@ -12,6 +12,20 @@ class DoublyLinkedList {
         this.head = null;
         this.tail = null;
     }
+
+    push (val) {
+        const node = new Node(val);
+        if (this.head === null) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+
+        this.length++;
+        return this;
+    }
 }
 
 const list = new DoublyLinkedList();
