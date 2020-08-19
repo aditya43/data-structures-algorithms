c00fb50c6ddf19bf198377f9ae1e29ac55cbe242
Aditya Hajare <aditya43@gmail.com>
2020-08-19 11:04:13 +0530

    Queue enqueue() method

--- 10-Queue-Using-Singly-Linked-List/queue.js
+++ 10-Queue-Using-Singly-Linked-List/queue.js
@@ -10,6 +10,29 @@ class Queue {
         this.size = 0;
         this.head = this.tail = null;
     }
+
+    enqueue (val) {
+        const node = new Node(val);
+
+        if (this.size === 0) {
+            this.head = this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+
+        return ++this.size;
+    }
 }
 
 const queue = new Queue();
+
+console.log(queue.enqueue('First'));
+console.log(queue.enqueue('Second'));
+console.log(queue.enqueue('Third'));
+console.log(queue.enqueue('Fourth'));
+console.log(queue.enqueue('Fifth'));
+console.log(queue.enqueue('Sixth'));
+console.log(queue.enqueue('Seventh'));
+console.log(queue.enqueue('Eighth'));
+console.log(queue.enqueue('Ninth'));
