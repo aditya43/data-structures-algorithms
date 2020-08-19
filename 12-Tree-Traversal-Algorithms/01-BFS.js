77f261d50b665c56ac55753663f3ab60ccf965e1
Aditya Hajare <aditya43@gmail.com>
2020-08-19 22:39:25 +0530

    BST breadthFirstSearch() method

--- 12-Tree-Traversal-Algorithms/01-BFS.js
+++ 12-Tree-Traversal-Algorithms/01-BFS.js
@@ -36,14 +36,47 @@ class BinarySearchTree {
             this.insert(val, current.right);
         }
     }
+
+    breadthFirstSearch () {
+        if (!this.root) {
+            return -1;
+        }
+
+        const queue = [this.root];
+        const data = [];
+
+        let node = this.root;
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.val);
+
+            if (node.left) {
+                queue.push(node.left);
+            }
+
+            if (node.right) {
+                queue.push(node.right);
+            }
+        }
+
+        return data;
+    }
 }
 
 const bst = new BinarySearchTree();
 
-bst.insert(3);
-bst.insert(2);
-bst.insert(5);
-bst.insert(8);
-bst.insert(1);
+//      10
+//   5     13
+// 2  7  11  16
 
-console.log(JSON.stringify(bst, 1));
+bst.insert(10);
+bst.insert(5);
+bst.insert(13);
+bst.insert(11);
+bst.insert(2);
+bst.insert(16);
+bst.insert(7);
+// console.log(JSON.stringify(bst, 1));
+
+console.log(bst.breadthFirstSearch());
