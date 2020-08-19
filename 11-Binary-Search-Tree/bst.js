54a609c0f98d10a843fae1abab9d33149ddb31e7
Aditya Hajare <aditya43@gmail.com>
2020-08-19 15:09:07 +0530

    BST insert() method

--- 11-Binary-Search-Tree/bst.js
+++ 11-Binary-Search-Tree/bst.js
@@ -9,6 +9,43 @@ class BinarySearchTree {
     constructor () {
         this.root = null;
     }
+
+    insert (val) {
+        if (this.root === null) {
+            this.root = new Node(val);
+        } else {
+            this.insertRecursive(val, this.root);
+        }
+
+        return this;
+    }
+
+    insertRecursive (val, current) {
+        if (val === current.val) {
+            // Duplicate! BST can't have duplicate values
+            return -1;
+        }
+
+        if (val < current.val) {
+            if (current.left !== null) {
+                this.insertRecursive(val, current.left);
+            }
+            current.left = new Node(val);
+        } else {
+            if (current.right !== null) {
+                this.insertRecursive(val, current.right);
+            }
+            current.right = new Node(val);
+        }
+    }
 }
 
 const bst = new BinarySearchTree();
+
+bst.insert(3);
+bst.insert(2);
+bst.insert(5);
+bst.insert(8);
+bst.insert(1);
+
+console.log(JSON.stringify(bst));
