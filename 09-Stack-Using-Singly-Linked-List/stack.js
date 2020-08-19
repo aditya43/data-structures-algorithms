61af50436a82970b249fcd047671c643d2dd9d8d
Aditya Hajare <aditya43@gmail.com>
2020-08-19 10:31:57 +0530

    Stack push method

--- 09-Stack-Using-Singly-Linked-List/stack.js
+++ 09-Stack-Using-Singly-Linked-List/stack.js
@@ -11,6 +11,19 @@ class Stack {
         this.first = null;
         this.last = null;
     }
+
+    push (val) {
+        const node = new Node(val);
+
+        if (this.size === 0) {
+            this.first = this.last = node;
+        } else {
+            node.next = this.first.next;
+            this.first = node;
+        }
+
+        return ++this.size;
+    }
 }
 
 const stack = new Stack();
