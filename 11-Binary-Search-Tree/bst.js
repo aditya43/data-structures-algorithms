f506d10f488673f31436c8e104239590a1b2f2ff
Aditya Hajare <aditya43@gmail.com>
2020-08-19 14:36:40 +0530

    bst skeleton

---
+++ 11-Binary-Search-Tree/bst.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor (val) {
+        this.val = val;
+        this.left = this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor () {
+        this.root = null;
+    }
+}
+
+const bst = new BinarySearchTree();
