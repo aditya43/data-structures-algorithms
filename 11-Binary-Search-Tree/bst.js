86ff9add670266c4034aa17afeae6d67934445fd
Aditya Hajare <aditya43@gmail.com>
2020-08-19 15:47:50 +0530

    BST insert() and search() optimized

--- 11-Binary-Search-Tree/bst.js
+++ 11-Binary-Search-Tree/bst.js
@@ -27,15 +27,38 @@ class BinarySearchTree {
         }
 
         if (val < current.val) {
-            if (current.left !== null) {
-                this.insertRecursive(val, current.left);
+            if (current.left === null) {
+                current.left = new Node(val);
             }
-            current.left = new Node(val);
+            this.insertRecursive(val, current.left);
         } else {
-            if (current.right !== null) {
-                this.insertRecursive(val, current.right);
+            if (current.right === null) {
+                current.right = new Node(val);
             }
-            current.right = new Node(val);
+            this.insertRecursive(val, current.right);
+        }
+    }
+
+    search (val) {
+        if (!this.root) {
+            return -1;
+        }
+
+        return this.findRecursive(val, this.root) || -1;
+    }
+
+    findRecursive (val, current) {
+        if (current.val === val) {
+            return true;
+        }
+        if (val < current.val) {
+            if (current.left) {
+                return this.findRecursive(val, current.left);
+            }
+        } else {
+            if (current.right) {
+                return this.findRecursive(val, current.right);
+            }
         }
     }
 }
@@ -48,4 +71,9 @@ bst.insert(5);
 bst.insert(8);
 bst.insert(1);
 
-console.log(JSON.stringify(bst));
+console.log(bst.search(3));
+console.log(bst.search(2));
+console.log(bst.search(5));
+console.log(bst.search(8));
+console.log(bst.search(1));
+console.log(bst.search(99));
