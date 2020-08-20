6f1b5ca61b8bcb027400ad47b928b6af3d1424b1
Aditya Hajare <aditya43@gmail.com>
2020-08-20 13:42:24 +0530

    Binary Heap

--- 13-Binary-Heaps/binaryHeap.js
+++ 13-Binary-Heaps/binaryHeap.js
@@ -9,7 +9,21 @@ class MaxBinaryHeap {
     }
 
     bubbleUp () {
+        let idx = this.values.length - 1;
 
+        while (idx > 0) {
+            const parentIdx = Math.floor((idx - 1) / 2);
+
+            if (this.values[idx] <= this.values[parentIdx]) {
+                break;
+            }
+
+            const tmp = this.values[parentIdx];
+            this.values[parentIdx] = this.values[idx];
+            this.values[idx] = tmp;
+
+            idx = parentIdx;
+        }
     }
 }
 
@@ -21,3 +35,10 @@ heap.insert(18);
 heap.insert(27);
 heap.insert(12);
 heap.insert(55);
+
+console.log(heap);
+
+// MaxBinaryHeap { values: [
+//     55, 39, 41, 18,
+//     27, 12, 33
+//   ] }
