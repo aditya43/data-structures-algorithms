fe58bef3f6a07a2a16f481b9e3a151a14c890e0e
Aditya Hajare <aditya43@gmail.com>
2020-08-20 14:45:00 +0530

    PriorityQueue

---
+++ 14-Priority-Queue-Using-MinHeap/priorityQueue.js
@@ -0,0 +1,104 @@
+class Node {
+    constructor (val, priority) {
+        this.val = val;
+        this.priority = priority;
+    }
+}
+
+class PriorityQueue {
+    constructor () {
+        this.values = [];
+    }
+
+    insert (val) {
+        this.values.push(val);
+        this.bubbleUp();
+    }
+
+    extractMax () {
+        const max = this.values[0];
+        const end = this.values.pop();
+
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.bubbleDown(0);
+        }
+
+        return max;
+    }
+
+    bubbleUp () {
+        let idx = this.values.length - 1;
+
+        while (idx > 0) {
+            const parentIdx = Math.floor((idx - 1) / 2);
+
+            if (this.values[idx] <= this.values[parentIdx]) {
+                break;
+            }
+
+            const tmp = this.values[parentIdx];
+            this.values[parentIdx] = this.values[idx];
+            this.values[idx] = tmp;
+
+            idx = parentIdx;
+        }
+    }
+
+    // Recursive
+    bubbleDown (index) {
+        const left = 2 * index + 1;
+        const right = 2 * index + 2;
+        let largest = index;
+        const length = this.values.length;
+
+        // if left child is greater than parent
+        if (left <= length && this.values[left] > this.values[largest]) {
+            largest = left;
+        }
+        // if right child is greater than parent
+        if (right <= length && this.values[right] > this.values[largest]) {
+            largest = right;
+        }
+        // swap
+        if (largest !== index) {
+            [this.values[largest], this.values[index]] = [this.values[index], this.values[largest]];
+            this.bubbleDown(largest);
+        }
+    }
+
+    // Iterative
+    sinkDown () {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            const leftChildIdx = 2 * idx + 1;
+            const rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild.priority < element.priority) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
+}
+
+const pQueue = new PriorityQueue();
