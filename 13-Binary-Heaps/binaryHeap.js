f523757e08cf23c75c1906ad341669b46e48291b
Aditya Hajare <aditya43@gmail.com>
2020-08-20 14:25:41 +0530

    MaxHeap Sink Down - Iterative

--- 13-Binary-Heaps/binaryHeap.js
+++ 13-Binary-Heaps/binaryHeap.js
@@ -59,6 +59,39 @@ class MaxBinaryHeap {
             this.bubbleDown(largest);
         }
     }
+
+    // Iterative
+    sinkDown () {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            const leftChildIdx = 2 * idx + 1;
+            const rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild.priority < element.priority) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild.priority < element.priority) ||
+                    (swap !== null && rightChild.priority < leftChild.priority)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
 }
 
 const heap = new MaxBinaryHeap();
