d13ee91dfd0b4f9e749f83a707b21bc80f220c8f
Aditya Hajare <aditya43@gmail.com>
2020-08-21 13:24:32 +0530

    Graph removeEdge() method

--- 17-Graphs/graph.js
+++ 17-Graphs/graph.js
@@ -17,6 +17,18 @@ class Graph {
             this.adjacencyList[vertex2].push(vertex1);
         }
     }
+
+    removeEdge (vertex1, vertex2) {
+        const v1 = this.adjacencyList[vertex1];
+        const v2 = this.adjacencyList[vertex2];
+
+        if (v1.indexOf(vertex2) >= 0) {
+            v1.splice(v1.indexOf(vertex2), 1);
+        }
+        if (v2.indexOf(vertex1) >= 0) {
+            v2.splice(v2.indexOf(vertex1), 1);
+        }
+    }
 }
 
 const graph = new Graph();
@@ -32,5 +44,9 @@ graph.addEdge('Pune', 'Goa');
 graph.addEdge('Pune', 'Mumbai');
 graph.addEdge('Mumbai', 'Delhi');
 graph.addEdge('Mumbai', 'Goa');
+
+console.log(graph);
+graph.removeEdge('Mumbai', 'Goa');
+graph.removeEdge('Pune', 'Goa');
 
 console.log(graph);
