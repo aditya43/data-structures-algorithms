3ff9bd6d1569f42db1beef1ae64d0d4d63f64692
Aditya Hajare <aditya43@gmail.com>
2020-08-21 07:13:50 +0530

    Hash table values() method

--- 16-Hash-Tables/hash-table.js
+++ 16-Hash-Tables/hash-table.js
@@ -47,6 +47,20 @@ class HashTable {
         }
         return keysArr;
     }
+
+    values () {
+        const valuesArr = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!valuesArr.includes(this.keyMap[i][j][1])) {
+                        valuesArr.push(this.keyMap[i][j][1]);
+                    }
+                }
+            }
+        }
+        return valuesArr;
+    }
 }
 
 const ht = new HashTable(17);
