d21ed864cb7377960e64883097682a20aa8b4827
Aditya Hajare <aditya43@gmail.com>
2020-08-21 13:16:42 +0530

    Graph addEdge() method

--- 17-Graphs/graph.js
+++ 17-Graphs/graph.js
@@ -8,6 +8,29 @@ class Graph {
             this.adjacencyList[vertex] = [];
         }
     }
+
+    addEdge (vertex1, vertex2) {
+        if (this.adjacencyList[vertex1].indexOf(vertex2) < 0) {
+            this.adjacencyList[vertex1].push(vertex2);
+        }
+        if (this.adjacencyList[vertex2].indexOf(vertex1) < 0) {
+            this.adjacencyList[vertex2].push(vertex1);
+        }
+    }
 }
 
 const graph = new Graph();
+
+// Add vertex
+graph.addVertex('Pune');
+graph.addVertex('Mumbai');
+graph.addVertex('Delhi');
+graph.addVertex('Goa');
+
+// Add edge
+graph.addEdge('Pune', 'Goa');
+graph.addEdge('Pune', 'Mumbai');
+graph.addEdge('Mumbai', 'Delhi');
+graph.addEdge('Mumbai', 'Goa');
+
+console.log(graph);
