46fbfd12d11ba8cb24ae247de6bb1d21d38d9206
Aditya Hajare <aditya43@gmail.com>
2020-08-21 07:11:58 +0530

    Hash table skeleton

---
+++ 16-Hash-Tables/hash-table.js
@@ -0,0 +1,17 @@
+class HashTable {
+    constructor (size = 53) {
+        this.keyMap = new Array(size);
+    }
+}
+
+const ht = new HashTable(17);
+
+ht.set('maroon', '#800000');
+ht.set('yellow', '#FFFF00');
+ht.set('olive', '#808000');
+ht.set('salmon', '#FA8072');
+ht.set('lightcoral', '#F08080');
+ht.set('mediumvioletred', '#C71585');
+ht.set('plum', '#DDA0DD');
+ht.set('purple', '#DDA0DD');
+ht.set('violet', '#DDA0DD');
