07ea0a0b0ed7bc4794958c2aaf4c6bd7a104b2d9
Aditya Hajare <aditya43@gmail.com>
2020-08-21 13:10:51 +0530

    Graph addVertex() method

--- 17-Graphs/graph.js
+++ 17-Graphs/graph.js
@@ -2,6 +2,12 @@ class Graph {
     constructor () {
         this.adjacencyList = {};
     }
+
+    addVertex (vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
 }
 
 const graph = new Graph();
