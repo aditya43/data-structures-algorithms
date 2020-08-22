b31254b19321cf2fb6670d62bfb4a80b94b90142
Aditya Hajare <aditya43@gmail.com>
2020-08-22 10:16:18 +0530

    Dijkstra skeleton

---
+++ 19-Dijkstras-Shortest-Path-Algorithm/dijkstra.js
@@ -0,0 +1,12 @@
+class Node {
+    constructor (vertex, weight) {
+        this.vertex = vertex;
+        this.weight = weight;
+    }
+}
+
+class WeightedGraph {
+    constructor () {
+        this.adjacencyList = {};
+    }
+}
