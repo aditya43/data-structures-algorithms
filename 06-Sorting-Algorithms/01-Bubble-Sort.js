e1a449d5e7db6c3353228892b079e982a98933c9
Aditya Hajare <aditya43@gmail.com>
2020-08-22 17:37:24 +0530

    Bubble sort

--- 06-Sorting-Algorithms/01-Bubble-Sort.js
+++ 06-Sorting-Algorithms/01-Bubble-Sort.js
@@ -6,6 +6,16 @@
  *
  * @author Aditya Hajare <https://github.com/aditya43>
  *
+ * IMPORTANT POINTS AND PSUDOCODE
+ * -----------------------------------
+ * A sorting algorithm where the largest values bubble up to the top!
+ *
+ * 1. Start looping from with a variable called i the end of the array
+ * towards the beginning
+ * 2. Start an inner loop with a variable called j from the beginning until i - 1
+ * 3. If arr[j] is greater than arr[j+1], swap those two values!
+ * 4. Return the sorted array
+ *
  * @param arr Array of integers
  */
 function unOptimizedBubbleSort (arr) {
