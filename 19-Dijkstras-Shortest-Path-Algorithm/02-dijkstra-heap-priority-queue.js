735fcadfbb99c5cdc98b6c1a8db54c7c3da5289d
Aditya Hajare <aditya43@gmail.com>
2020-08-22 17:31:41 +0530

    Dijkstras shortest path algo using Heap - priority queue

--- 19-Dijkstras-Shortest-Path-Algorithm/02-dijkstra-heap-priority-queue.js
+++ 19-Dijkstras-Shortest-Path-Algorithm/02-dijkstra-heap-priority-queue.js
@@ -1,7 +1,14 @@
-class Node {
+class Vertex {
     constructor (vertex, weight) {
-        this.vertex = vertex;
+        this.node = vertex;
         this.weight = weight;
+    }
+}
+
+class Node {
+    constructor (val, priority) {
+        this.val = val;
+        this.priority = priority;
     }
 }
 
@@ -10,17 +17,102 @@ class PriorityQueue {
         this.values = [];
     }
 
-    enqueue (val) {
-        this.values.push(val);
-        this.sort();
+    enqueue (val, priority) {
+        const node = new Node(val, priority);
+        this.values.push(node);
+        this.bubbleUp();
     }
 
     dequeue () {
-        return this.values.shift();
+        const min = this.values[0];
+        const end = this.values.pop();
+
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.bubbleDown(0);
+        }
+
+        return min;
     }
 
-    sort () {
-        this.values.sort((a, b) => a.priority - b.priority);
+    bubbleUp () {
+        let idx = this.values.length - 1;
+
+        while (idx > 0) {
+            const parentIdx = Math.floor((idx - 1) / 2);
+
+            if (this.values[idx].priority >= this.values[parentIdx].priority) {
+                break;
+            }
+
+            const tmp = this.values[parentIdx];
+            this.values[parentIdx] = this.values[idx];
+            this.values[idx] = tmp;
+
+            idx = parentIdx;
+        }
+    }
+
+    // Recursive
+    bubbleDown (index) {
+        const length = this.values.length;
+        let largest = index;
+
+        const left = 2 * index + 1;
+        const right = 2 * index + 2;
+
+        // if left child is greater than parent
+        if (left <= length && this.values[left]) {
+            if (this.values[left].priority < this.values[largest].priority) {
+                largest = left;
+            }
+        }
+
+        // if right child is greater than parent
+        if (right <= length && this.values[right]) {
+            if (this.values[right].priority < this.values[largest].priority) {
+                largest = right;
+            }
+        }
+
+        // swap
+        if (largest !== index) {
+            [this.values[largest], this.values[index]] = [this.values[index], this.values[largest]];
+            this.bubbleDown(largest);
+        }
+    }
+
+    // Iterative
+    bubbleDownIterative () {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while (true) {
+            const leftChildIdx = 2 * idx + 1;
+            const rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild.priority > element.priority) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild.priority > element.priority) ||
+                    (swap !== null && rightChild.priority > leftChild.priority)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
     }
 }
 
@@ -36,8 +128,8 @@ class WeightedGraph {
     }
 
     addEdge (vertex1, vertex2, weight) {
-        this.adjacencyList[vertex1].push(new Node(vertex2, weight));
-        this.adjacencyList[vertex2].push(new Node(vertex1, weight));
+        this.adjacencyList[vertex1].push(new Vertex(vertex2, weight));
+        this.adjacencyList[vertex2].push(new Vertex(vertex1, weight));
     }
 
     Dijkstra (start, finish) {
@@ -109,4 +201,4 @@ graph.addEdge('D', 'E', 3);
 graph.addEdge('D', 'F', 1);
 graph.addEdge('E', 'F', 1);
 
-console.log(JSON.stringify(graph, null, 4));
+console.log(graph.Dijkstra('A', 'E'));
