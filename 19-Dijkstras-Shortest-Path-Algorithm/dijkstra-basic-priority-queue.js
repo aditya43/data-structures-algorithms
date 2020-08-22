fb06d581609b2289f22c98a56c0949b31db3e0e4
Aditya Hajare <aditya43@gmail.com>
2020-08-22 17:20:21 +0530

    Dijkstras shortest path algorithm using basic priority queue

--- 19-Dijkstras-Shortest-Path-Algorithm/dijkstra-basic-priority-queue.js
+++ 19-Dijkstras-Shortest-Path-Algorithm/dijkstra-basic-priority-queue.js
@@ -11,15 +11,16 @@ class PriorityQueue {
     }
 
     enqueue (val) {
-
+        this.values.push(val);
+        this.sort();
     }
 
     dequeue () {
-
+        return this.values.shift();
     }
 
     sort () {
-
+        this.values.sort((a, b) => a.priority - b.priority);
     }
 }
 
@@ -37,6 +38,56 @@ class WeightedGraph {
     addEdge (vertex1, vertex2, weight) {
         this.adjacencyList[vertex1].push(new Node(vertex2, weight));
         this.adjacencyList[vertex2].push(new Node(vertex1, weight));
+    }
+
+    Dijkstra (start, finish) {
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        const path = []; // to return at end
+        let smallest;
+        // build up initial state
+        for (const vertex in this.adjacencyList) {
+            if (vertex === start) {
+                distances[vertex] = 0;
+                nodes.enqueue(vertex, 0);
+            } else {
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex, Infinity);
+            }
+            previous[vertex] = null;
+        }
+        // as long as there is something to visit
+        while (nodes.values.length) {
+            smallest = nodes.dequeue().val;
+            if (smallest === finish) {
+                // WE ARE DONE
+                // BUILD UP PATH TO RETURN AT END
+                while (previous[smallest]) {
+                    path.push(smallest);
+                    smallest = previous[smallest];
+                }
+                break;
+            }
+            if (smallest || distances[smallest] !== Infinity) {
+                for (const neighbor in this.adjacencyList[smallest]) {
+                    // find neighboring node
+                    const nextNode = this.adjacencyList[smallest][neighbor];
+                    // calculate new distance to neighboring node
+                    const candidate = distances[smallest] + nextNode.weight;
+                    const nextNeighbor = nextNode.node;
+                    if (candidate < distances[nextNeighbor]) {
+                        // updating new smallest distance to neighbor
+                        distances[nextNeighbor] = candidate;
+                        // updating previous - How we got to neighbor
+                        previous[nextNeighbor] = smallest;
+                        // enqueue in priority queue with new priority
+                        nodes.enqueue(nextNeighbor, candidate);
+                    }
+                }
+            }
+        }
+        return path.concat(smallest).reverse();
     }
 }
 
