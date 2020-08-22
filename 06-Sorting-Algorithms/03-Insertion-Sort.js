76647dad6c7308a80bbadf9228f70c6fceb2ed67
Aditya Hajare <aditya43@gmail.com>
2020-08-22 17:40:44 +0530

    Insertion sort

--- 06-Sorting-Algorithms/03-Insertion-Sort.js
+++ 06-Sorting-Algorithms/03-Insertion-Sort.js
@@ -6,6 +6,16 @@
  *
  * @author Aditya Hajare <https://github.com/aditya43>
  *
+ * IMPORTANT POINTS AND PSUDOCODE
+ * -----------------------------------
+ * Builds up the sort by gradually creating a larger left half which is always sorted.
+ *
+ * 1. Start by picking the second element in the array
+ * 2. Now compare the second element with the one before it and swap if necessary.
+ * 3. Continue to the next element and if it is in the incorrect order, iterate
+ * through the sorted portion (i.e. the left side) to place the element in the correct place.
+ * 4. Repeat until the array is sorted.
+ *
  * @param arr Array of integers
  */
 function insertionSort (arr) {
