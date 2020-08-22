ea861742c5816a17c4762166a4aca7c709311f16
Aditya Hajare <aditya43@gmail.com>
2020-08-22 18:06:38 +0530

    Binary Search

--- 07-Searching-Algorithms/02-Binary-Search.js
+++ 07-Searching-Algorithms/02-Binary-Search.js
@@ -9,6 +9,25 @@
  *
  * @author Aditya Hajare <https://github.com/aditya43>
  *
+ * IMPORTANT POINTS AND PSUDOCODE
+ * -----------------------------------
+ * Binary search is a much faster form of search
+ * Rather than eliminating one element at a time, you can eliminate half
+ * of the remaining elements at a time
+ * Binary search only works on sorted arrays!
+ *
+ * Binary Search Pseudocode
+ * -----------------------------------
+ * 1. This function accepts a sorted array and a value
+ * 2. Create a left pointer at the start of the array, and a right
+ * pointer at the end of the array
+ * 3. While the left pointer <= right pointer:
+ *  - Create a pointer in the middle
+ *  - If you find the value you want, return the index
+ *  - If the value is too small, move the left pointer up
+ *  - If the value is too large, move the right pointer down
+ * 4. If you never find the value, return -1
+ *
  * @param arr Sorted array of integers
  * @param num Number to search
  */
