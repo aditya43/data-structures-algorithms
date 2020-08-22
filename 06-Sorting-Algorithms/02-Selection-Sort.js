206cf9c927eb7fe649cc3ae155217d365fb9dc92
Aditya Hajare <aditya43@gmail.com>
2020-08-22 17:39:23 +0530

    Selection sort

--- 06-Sorting-Algorithms/02-Selection-Sort.js
+++ 06-Sorting-Algorithms/02-Selection-Sort.js
@@ -6,6 +6,18 @@
  *
  * @author Aditya Hajare <https://github.com/aditya43>
  *
+ * IMPORTANT POINTS AND PSUDOCODE
+ * -----------------------------------
+ * Similar to bubble sort, but instead of first placing large values
+ * into sorted position, it places small values into sorted position
+ *
+ * 1. Store the first element as the smallest value you've seen so far.
+ * 2. Compare this item to the next item in the array until you find a smaller number.
+ * 3. If a smaller number is found, designate that smaller number to be
+ * the new "minimum" and continue until the end of the array.
+ * 4. If the "minimum" is not the value (index) you initially began with, swap the two values.
+ * 5.Repeat this with the next element until the array is sorted.
+ *
  * @param arr Array of integers
  */
 function selectionSort (arr) {
