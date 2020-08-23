bbc3ffbdaa2abc1526504ccb1fbb26e51566a66b
Aditya Hajare <aditya43@gmail.com>
2020-08-23 08:30:30 +0530

    Insertion sort

--- 06-Sorting-Algorithms/03-Insertion-Sort.js
+++ 06-Sorting-Algorithms/03-Insertion-Sort.js
@@ -13,7 +13,8 @@
  * 1. Start by picking the second element in the array
  * 2. Now compare the second element with the one before it and swap if necessary.
  * 3. Continue to the next element and if it is in the incorrect order, iterate
- * through the sorted portion (i.e. the left side) to place the element in the correct place.
+ * through the sorted portion (i.e. the left side) to place the element in
+ * the correct place.
  * 4. Repeat until the array is sorted.
  *
  * @param arr Array of integers
