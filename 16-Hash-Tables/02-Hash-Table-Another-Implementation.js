f913e42be10809d0332c23fae6c8165adb917e38
Aditya Hajare <aditya43@gmail.com>
2020-08-26 17:15:22 +0530

    Hash table another implementation

---
+++ 16-Hash-Tables/02-Hash-Table-Another-Implementation.js
@@ -0,0 +1,74 @@
+/* Hash Table */
+
+const hash = (string, max) => {
+    let hash = 0;
+    for (let i = 0; i < string.length; i++) {
+        hash += string.charCodeAt(i);
+    }
+    return hash % max;
+};
+
+const HashTable = function () {
+    const storage = [];
+    const storageLimit = 14;
+
+    this.print = function () {
+        console.log(storage);
+    };
+
+    this.add = function (key, value) {
+        const index = hash(key, storageLimit);
+        if (storage[index] === undefined) {
+            storage[index] = [
+                [key, value]
+            ];
+        } else {
+            let inserted = false;
+            for (let i = 0; i < storage[index].length; i++) {
+                if (storage[index][i][0] === key) {
+                    storage[index][i][1] = value;
+                    inserted = true;
+                }
+            }
+            if (inserted === false) {
+                storage[index].push([key, value]);
+            }
+        }
+    };
+
+    this.remove = function (key) {
+        const index = hash(key, storageLimit);
+        if (storage[index].length === 1 && storage[index][0][0] === key) {
+            delete storage[index];
+        } else {
+            for (let i = 0; i < storage[index].length; i++) {
+                if (storage[index][i][0] === key) {
+                    delete storage[index][i];
+                }
+            }
+        }
+    };
+
+    this.lookup = function (key) {
+        const index = hash(key, storageLimit);
+        if (storage[index] === undefined) {
+            return undefined;
+        } else {
+            for (let i = 0; i < storage[index].length; i++) {
+                if (storage[index][i][0] === key) {
+                    return storage[index][i][1];
+                }
+            }
+        }
+    };
+};
+
+console.log(hash('quincy', 10));
+
+const ht = new HashTable();
+ht.add('aditya', 'person');
+ht.add('fido', 'dog');
+ht.add('rex', 'dinosour');
+ht.add('tux', 'penguin');
+console.log(ht.lookup('tux'));
+ht.print();
