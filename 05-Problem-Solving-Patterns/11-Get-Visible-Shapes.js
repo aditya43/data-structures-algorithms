38906d27aef42a5f3bc655e7d4f179ed8639812b
Aditya Hajare <aditya43@gmail.com>
2020-08-27 07:49:10 +0530

    Get visible shapes in 2D space

---
+++ 05-Problem-Solving-Patterns/11-Get-Visible-Shapes.js
@@ -0,0 +1,39 @@
+/**
+ * Visible Mountains
+ *
+ *
+ * @author Aditya Hajare <https://github.com/aditya43>
+ *
+ * @param triangles 2D Space coordinates
+ */
+
+function getVisibleMountains (triangles) {
+    let visible = triangles.length;
+
+    for (const i in triangles) {
+        curMountLeft = triangles[i][0] - triangles[i][1];
+        curMountRight = triangles[i][0] + triangles[i][1];
+
+        for (const j in triangles) {
+            otherMountLeft = triangles[j][0] - triangles[j][1];
+            otherMountRight = triangles[j][0] + triangles[j][1];
+
+            if (i === j) {
+                continue;
+            }
+
+            if (curMountLeft >= otherMountLeft && curMountRight <= otherMountRight) {
+                visible -= 1;
+                break;
+            }
+        }
+    }
+
+    console.log(visible);
+}
+
+getVisibleMountains([
+    [4, 6],
+    [7, 2],
+    [2, 5]
+]);
