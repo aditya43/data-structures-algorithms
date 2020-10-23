b008a126d8f72eb67a141886cadb9c4b90ce8bde
Aditya Hajare <aditya43@gmail.com>
2020-10-23 18:34:18 +0530

    Reverse string

--- 25-Simple-Problems/01-String-Reverse.js
+++ 25-Simple-Problems/01-String-Reverse.js
@@ -2,4 +2,14 @@ function solutionOne(str) {
     return str.split('').reverse().join('');
 }
 
+function solutionTwo(str) {
+    let res = '';
+
+    for (const chr of str) {
+        res = chr + res;
+    }
+
+    return res;
+}
+
 console.log(solutionOne('aditya'));
