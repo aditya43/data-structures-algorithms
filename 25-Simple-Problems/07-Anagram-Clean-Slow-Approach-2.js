bb0c8b82416d01a650824a5babe5cc5530a94fe7
Aditya Hajare <aditya43@gmail.com>
2020-10-24 12:00:47 +0530

    Anagram approcah 2

---
+++ 25-Simple-Problems/07-Anagram-Clean-Slow-Approach-2.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// An anagram is a word, phrase or name formed by rearranging the letters
+// of another, such as "cinema", formed from "iceman".
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams(str1, str2) {}
+
+console.log(anagrams('rail safety', 'fairy tales')); // True;
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales')); // True;
+console.log(anagrams('Hi there', 'Bye there')); // False;
