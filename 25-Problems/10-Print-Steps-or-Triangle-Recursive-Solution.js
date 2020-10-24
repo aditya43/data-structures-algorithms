5e9c3ac2e18d6201003849a8fc2b3be177ea7775
Aditya Hajare <aditya43@gmail.com>
2020-10-24 17:23:42 +0530

    Recursive solution

--- 25-Problems/10-Print-Steps-or-Triangle-Recursive-Solution.js
+++ 25-Problems/10-Print-Steps-or-Triangle-Recursive-Solution.js
@@ -17,20 +17,18 @@
 //       '### '
 //       '####'
 
-function steps(n) {
-    for (let row = 0; row < n; row++) {
-        let stair = '';
-
-        for (let col = 0; col < n; col++) {
-            if (col <= row) {
-                stair += '#';
-            } else {
-                stair += ' ';
-            }
-        }
-
-        console.log(stair);
+function steps(n, row = 0, stair = '') {
+    if (n === row) {
+        return;
     }
+
+    if (n === stair.length) {
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    stair += stair.length <= row ? '#' : ' ';
+    steps(n, row, stair);
 }
 
 steps(10);
