eecd72f8acb4c922c1af6dd5f7b91248a94a9425
Aditya Hajare <aditya43@gmail.com>
2020-10-25 07:41:31 +0530

    USing RegEX

---
+++ 25-Problems/14-Find-All-Vowels-In-String-Using-RegEX.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
+}
+
+console.log(vowels('Hi There!')); // 3
+console.log(vowels('Why do you ask?')); // 4
+console.log(vowels('Why?')); // 0
