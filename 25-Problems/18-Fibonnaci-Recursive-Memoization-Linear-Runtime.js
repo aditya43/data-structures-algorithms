33aa318f3c8c9138f6baec79dda5429d6e9a64cf
Aditya Hajare <aditya43@gmail.com>
2020-10-26 10:23:47 +0530

    Recursive Solution | Linear Time | Memoized

---
+++ 25-Problems/18-Fibonnaci-Recursive-Memoization-Linear-Runtime.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n, res = [0, 1, 1]) {
+    if (res[n]) {
+        return res[n];
+    }
+
+    res[n] = fib(n - 1, res) + fib(n - 2, res);
+    return res[n];
+}
+
+console.log(fib(155));
