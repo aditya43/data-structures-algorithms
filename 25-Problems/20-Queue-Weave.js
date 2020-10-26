5498b9f2c328180880023a6da42a6108f7a53be1
Aditya Hajare <aditya43@gmail.com>
2020-10-26 15:43:41 +0530

    Queue

---
+++ 25-Problems/20-Queue-Weave.js
@@ -0,0 +1,32 @@
+// Implement the 'weave' function.  Weave
+// receives two queues as arguments and combines the
+// contents of each into a new, third queue.
+// The third queue should contain the *alterating* content
+// of the two queues.  The function should handle
+// queues of different lengths without inserting
+// 'undefined' into the new one.
+//    const queueOne = new Queue();
+//    queueOne.add(1);
+//    queueOne.add(2);
+//    const queueTwo = new Queue();
+//    queueTwo.add('Hi');
+//    queueTwo.add('There');
+//    const q = weave(queueOne, queueTwo);
+//    q.remove() // 1
+//    q.remove() // 'Hi'
+//    q.remove() // 2
+//    q.remove() // 'There'
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+}
