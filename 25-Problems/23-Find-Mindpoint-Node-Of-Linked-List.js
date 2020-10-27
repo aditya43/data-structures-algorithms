beefd9020cf56a1fc0600ef59a4e59c6f8d0f6ed
Aditya Hajare <aditya43@gmail.com>
2020-10-27 12:00:16 +0530

    Calculate midpoint of a linked list

--- 25-Problems/23-Find-Mindpoint-Node-Of-Linked-List.js
+++ 25-Problems/23-Find-Mindpoint-Node-Of-Linked-List.js
@@ -14,5 +14,38 @@
 
 const LinkedList = require('./Data-Structures/01-Linked-List');
 
+function midPoint(ll) {
+    let slow = ll.head;
+    let fast = ll.head;
+
+    while (fast.next && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return slow;
+}
+
 const ll = new LinkedList();
-ll.add(1).add(2).add(3).add(4);
+ll.add(1)
+    .add(2)
+    .add(3)
+    .add(4)
+    .add(5)
+    .add(6)
+    .add(7)
+    .add(8)
+    .add(9)
+    .add(10)
+    .add(11)
+    .add(12)
+    .add(13)
+    .add(14)
+    .add(15)
+    .add(16)
+    .add(17)
+    .add(18)
+    .add(19)
+    .add(20);
+
+console.log(midPoint(ll));
