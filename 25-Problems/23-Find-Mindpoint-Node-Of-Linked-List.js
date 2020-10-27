a67a68cbfd313d906ca61f4ad843d0ab7e06b7ec
Aditya Hajare <aditya43@gmail.com>
2020-10-27 11:13:01 +0530

    Linked list

---
+++ 25-Problems/23-Find-Mindpoint-Node-Of-Linked-List.js
@@ -0,0 +1,54 @@
+// --- Directions
+// Return the 'middle' node of a linked list.
+// If the list has an even number of elements, return
+// the node at the end of the first half of the list.
+// *Do not* use a counter variable, *do not* retrieve
+// the size of the list, and only iterate
+// through the list one time.
+// --- Example
+//   const l = new LinkedList();
+//   l.insertLast('a')
+//   l.insertLast('b')
+//   l.insertLast('c')
+//   midpoint(l); // returns { data: 'b' }
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = this.tail = null;
+    }
+
+    add(val) {
+        const node = new Node(val);
+
+        if (!this.head) {
+            this.head = this.tail = node;
+            return this;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+        return this;
+    }
+
+    remove() {
+        const node = this.head;
+
+        if (!this.head || !this.head.next) {
+            this.head = this.tail = null;
+        } else {
+            this.head = this.head.next;
+        }
+
+        return node;
+    }
+}
+
+const ll = new LinkedList();
+ll.add(1).add(2).add(3).add(4);
