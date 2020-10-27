6e8e4c7f4ea498c360cd8d69d150fb6c14018db4
Aditya Hajare <aditya43@gmail.com>
2020-10-27 11:04:19 +0530

    Linked list add()

--- 25-Problems/22-Linked-List-With-Iterator-Generator.js
+++ 25-Problems/22-Linked-List-With-Iterator-Generator.js
@@ -6,3 +6,22 @@ class Node {
         this.next = nulll;
     }
 }
+
+class LinkedList {
+    constructor() {
+        this.head = this.tail = null;
+    }
+
+    add(val) {
+        const node = new Node(val);
+
+        if (!this.head) {
+            this.head = this.tail = node;
+            return this;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+        return this;
+    }
+}
