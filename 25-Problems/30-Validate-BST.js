a3a56f94077c5c3f0ab283085dab0553ae713642
Aditya Hajare <aditya43@gmail.com>
2020-10-29 07:32:17 +0530

    BST

---
+++ 25-Problems/30-Validate-BST.js
@@ -0,0 +1,26 @@
+// --- Directions
+// Given a node, validate the binary search tree,
+// ensuring that every node's left hand child is
+// less than the parent node's value, and that
+// every node's right hand child is greater than
+// the parent
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(data) {
+        if (data < this.data && this.left) {
+            this.left.insert(data);
+        } else if (data < this.data) {
+            this.left = new Node(data);
+        } else if (data > this.data && this.right) {
+            this.right.insert(data);
+        } else if (data > this.data) {
+            this.right = new Node(data);
+        }
+    }
+}
