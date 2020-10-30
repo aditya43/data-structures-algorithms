7f8d072afc59ad8faf7d31096bc10bab37cace2a
Aditya Hajare <aditya43@gmail.com>
2020-10-30 19:09:01 +0530

    LeetCode #416

--- 26-LeetCode/03-Partition-Equal-Subset-Sum-LC-416.js
+++ 26-LeetCode/03-Partition-Equal-Subset-Sum-LC-416.js
@@ -14,3 +14,23 @@ Explanation: The array cannot be partitioned into equal sum subsets.
 */
 
 // Solved using Dynamic Programming
+
+var canPartition = function (nums) {
+    let sum = 0;
+    for (let num of nums) {
+        sum += num;
+    }
+    if (sum % 2 != 0) return false;
+    let half = sum / 2;
+    let dp = new Array(half + 1).fill(false);
+    dp[0] = true;
+    for (let num of nums) {
+        for (let i = half; i >= num; i--) {
+            dp[i] = dp[i] || dp[i - num];
+        }
+    }
+    return dp[half];
+};
+
+console.log(canPartition([1, 5, 11, 5])); // true
+console.log(canPartition([1, 2, 3, 5])); // false
