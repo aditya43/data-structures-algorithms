7798c29779d3002a03ce7334751c189ae2b4f2c8
Aditya Hajare <aditya43@gmail.com>
2020-10-30 19:08:05 +0530

    LeetCode #416

---
+++ 26-LeetCode/03-Partition-Equal-Subset-Sum-LC-416.js
@@ -0,0 +1,16 @@
+/*
+416. Partition Equal Subset Sum
+Medium
+
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+*/
+
+// Solved using Dynamic Programming
