37693477d0b920dd687c35bbbd9a07178dd1b95e
Aditya Hajare <aditya43@gmail.com>
2020-10-30 10:43:03 +0530

    Refactored

--- 05-Problem-Solving-Patterns/13-Get-Visible-Shapes-Optimized-Sorted-Input.js
+++ 05-Problem-Solving-Patterns/13-Get-Visible-Shapes-Optimized-Sorted-Input.js
@@ -7,14 +7,14 @@
  * @param triangles Array of triangle coordinates in 2D Space (x, y)
  */
 
-function getVisibleMountains(triangles) {
-    triangles.sort((a, b) => a[1] - b[1] - a[0] - b[0]);
+function getVisibleMountains (triangles) {
+    triangles.sort((a, b) => (a[1] - b[1]) - a[0] - b[0]);
     const lookupTable = {};
 
     for (const [i, triangle] in triangles) {
         lookupTable[i] = {
             X1: triangle[0] - triangle[1],
-            X2: triangle[0] + triangle[1],
+            X2: triangle[0] + triangle[1]
         };
     }
 
@@ -23,11 +23,7 @@ function getVisibleMountains(triangles) {
         const right = triangle[0] + triangle[1];
 
         for (let j = triangles.length - 1; j > i; j--) {
-            if (
-                lookupTable[j] &&
-                left >= lookupTable[j].X1 &&
-                right <= lookupTable[j].X2
-            ) {
+            if (lookupTable[j] && left >= lookupTable[j].X1 && right <= lookupTable[j].X2) {
                 delete lookupTable[i];
                 break;
             }
@@ -42,7 +38,7 @@ const visible = getVisibleMountains([
     [7, 2],
     [7, 2],
     [2, 5],
-    [2, 5],
+    [2, 5]
 ]);
 
 console.log(visible);
