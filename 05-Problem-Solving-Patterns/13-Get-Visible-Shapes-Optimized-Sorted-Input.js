b308e04cedb168d0c205a668513281a127b471f0
Aditya Hajare <aditya43@gmail.com>
2020-10-30 10:42:46 +0530

    Refactored

--- 05-Problem-Solving-Patterns/13-Get-Visible-Shapes-Optimized-Sorted-Input.js
+++ 05-Problem-Solving-Patterns/13-Get-Visible-Shapes-Optimized-Sorted-Input.js
@@ -7,23 +7,27 @@
  * @param triangles Array of triangle coordinates in 2D Space (x, y)
  */
 
-function getVisibleMountains (triangles) {
-    triangles.sort((a, b) => (a[1] - b[1]) - a[0] - b[0]);
+function getVisibleMountains(triangles) {
+    triangles.sort((a, b) => a[1] - b[1] - a[0] - b[0]);
     const lookupTable = {};
 
-    for (const i in triangles) {
+    for (const [i, triangle] in triangles) {
         lookupTable[i] = {
-            X1: triangles[i][0] - triangles[i][1],
-            X2: triangles[i][0] + triangles[i][1]
+            X1: triangle[0] - triangle[1],
+            X2: triangle[0] + triangle[1],
         };
     }
 
-    for (let i = 0; i < triangles.length; i++) {
-        const left = triangles[i][0] - triangles[i][1];
-        const right = triangles[i][0] + triangles[i][1];
+    for (const [i, triangle] of triangles) {
+        const left = triangle[0] - triangle[1];
+        const right = triangle[0] + triangle[1];
 
         for (let j = triangles.length - 1; j > i; j--) {
-            if (lookupTable[j] && left >= lookupTable[j].X1 && right <= lookupTable[j].X2) {
+            if (
+                lookupTable[j] &&
+                left >= lookupTable[j].X1 &&
+                right <= lookupTable[j].X2
+            ) {
                 delete lookupTable[i];
                 break;
             }
@@ -38,7 +42,7 @@ const visible = getVisibleMountains([
     [7, 2],
     [7, 2],
     [2, 5],
-    [2, 5]
+    [2, 5],
 ]);
 
 console.log(visible);
