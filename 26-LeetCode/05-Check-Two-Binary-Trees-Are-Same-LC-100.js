d8903d36fa76524049f0c030c7a1f5f6a20a31c9
Aditya Hajare <aditya43@gmail.com>
2020-11-07 17:58:59 +0530

    LC #100

---
+++ 26-LeetCode/05-Check-Two-Binary-Trees-Are-Same-LC-100.js
@@ -0,0 +1,32 @@
+/*
+100. Same Tree
+Easy
+
+Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+*/
