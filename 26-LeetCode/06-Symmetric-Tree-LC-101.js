3b1e722c4604cab4609a2bf72da29e55466630d8
Aditya Hajare <aditya43@gmail.com>
2020-11-08 17:05:09 +0530

    LC #101

--- 26-LeetCode/06-Symmetric-Tree-LC-101.js
+++ 26-LeetCode/06-Symmetric-Tree-LC-101.js
@@ -19,3 +19,58 @@ But the following [1,2,2,null,3,null,3] is not:
 
 Follow up: Solve it both recursively and iteratively.
 */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function (root) {
+    var queue = [];
+    queue.push(root);
+
+    while (queue.length !== 0) {
+        var len = queue.length;
+
+        if (!isLevelSymmetric(queue)) {
+            return false;
+        }
+
+        for (var i = 0; i < len; i++) {
+            var node = queue.shift();
+
+            if (node !== null) {
+                queue.push(node.left);
+                queue.push(node.right);
+            }
+        }
+    }
+
+    return true;
+};
+
+function isLevelSymmetric(nodes) {
+    var len = nodes.length;
+    var beg = 0;
+    var end = len - 1;
+
+    while (beg < end) {
+        if (
+            (nodes[beg] === null && nodes[end] === null) ||
+            (nodes[beg] && nodes[end] && nodes[beg].val === nodes[end].val)
+        ) {
+            beg++;
+            end--;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
