12cfd7f0bb12d01c45d604640bf377d638cdabf9
Aditya Hajare <aditya43@gmail.com>
2020-11-14 19:00:32 +0530

    LC #104

--- 26-LeetCode/09-Maximum-Depth-Of-Binary-Tree-LC-104.js
+++ 26-LeetCode/09-Maximum-Depth-Of-Binary-Tree-LC-104.js
@@ -15,3 +15,22 @@ Given binary tree [3,9,20,null,null,15,7],
 
 return its depth = 3.
 */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+    if (root === null) {
+        return 0;
+    }
+
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
